c3dcc0d5b16a3226e472f5b26f1e5485aa514d47
parthaMalladi <partha.malladi@gmail.com>
2024-10-25 23:43:43 +0000

    Saving Work

--- DateServerMT.java
+++ DateServerMT.java
@@ -1,16 +1,39 @@
 import java.net.*;
 import java.io.*;
 
-public class DateServer430
+public class DateServerMT
 {
     public static void main(String[] args)
     {
+        try {
+            ServerSocket sock = new ServerSocket(6013);
+
+            while (true) {
+                Socket client = sock.accept();
+                Thread myThread = new Thread(new Worker(client));
+                myThread.start();
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
+}
+
+// worker class to handle clients as threads
+class Worker implements Runnable {
+    private Socket client;
+
+    public Worker(Socket clientSocket) {
+        System.out.println("New Client Connected");
+        this.client = clientSocket;
+    }
+
+    @Override
+    public void run() {
         try
         {
-            ServerSocket sock = new ServerSocket(6013);
             while (true)
             {
-                Socket client = sock.accept();
                 PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
 		try
 		{
--- DateServerMTP.java
+++ DateServerMTP.java
@@ -1,17 +1,41 @@
 import java.net.*;
 import java.io.*;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
 
-public class DateServer430
+public class DateServerMTP
 {
     public static void main(String[] args)
     {
+        try {
+            ServerSocket sock = new ServerSocket(6013);
+            ExecutorService threadExecutor = Executors.newCachedThreadPool();
+
+            while (true) {
+                Socket client = sock.accept();
+                threadExecutor.execute(new Worker(client));
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
+}
+
+// worker class to handle clients as threads
+class Worker implements Runnable {
+    private Socket client;
+
+    public Worker(Socket clientSocket) {
+        System.out.println("New Client Connected");
+        this.client = clientSocket;
+    }
+
+    @Override
+    public void run() {
         try
         {
-            ServerSocket sock = new ServerSocket(6013);
             while (true)
             {
-                Socket client = sock.accept();
                 PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
 		try
 		{
